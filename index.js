5632b551c3847c7893dce011fd2cbe7f4172d853
Dk2626 <dineshkumardssd@gmail.com>
2024-02-06 22:51:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// backend/app.js
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3001; // Adjust as needed
+
+app.use(cors()); // Enable CORS
+
+app.get('/api/data', (req, res) => {
+  res.json({ message: 'Hello from the backend!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
